e65f5a94b44eef570a053bffc9b522774b89648f
Mikulew <mikulew@gmail.com>
2021-05-31 22:49:17 +0200

    Add points/maxPoints

--- src/features/quiz/Quiz.js
+++ src/features/quiz/Quiz.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { selectActiveQuestion, selectQuestions, next, toggle } from './quizSlice';
+import {
+  selectActiveQuestion,
+  selectQuestions,
+  selectFinished,
+  selectPoints,
+  selectMaxPoints,
+  next,
+  toggle,
+  finish
+} from './quizSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -11,36 +20,51 @@ import { Button } from '@material-ui/core'
 
 const Quiz = () => {
   const activeQuestion = useSelector(selectActiveQuestion);
+  const finished = useSelector(selectFinished);
   const questions = useSelector(selectQuestions);
+  const points = useSelector(selectPoints);
+  const maxPoints = useSelector(selectMaxPoints);
   const dispatch = useDispatch();
-  const handleNext = () => questions[activeQuestion + 1] ? dispatch(next()) : console.log('finished!');
+  const handleNext = () => {
+    if (finished) {
+      return;
+    }
+
+    return questions[activeQuestion + 1] ? dispatch(next()) : dispatch(finish());
+  };
   const handleToggle = answerIndex => () => { dispatch(toggle(answerIndex)) };
 
   return <div>
-    <p>{activeQuestion + 1} / {questions.length}</p>
-    <p>{questions[activeQuestion].content}</p>
-    <List>
-      {questions[activeQuestion].answers.map((answer, index) =>
-        <ListItem dense button onClick={handleToggle(index)} key={answer.id}>
-          <ListItemIcon>
-            <Checkbox
-              edge="start"
-              checked={answer.checked}
-              tabIndex={-1}
-              disableRipple
-              inputProps={{ 'aria-labelledby': answer.id }}
-            />
-          </ListItemIcon>
-          <ListItemText id={answer.id} primary={answer.content}/>
-        </ListItem>
-      )}
-    </List>
+    {!finished && <>
+      <p>{activeQuestion + 1} / {questions.length}</p>
+      <p>{questions[activeQuestion].content}</p>
+      <List>
+        {questions[activeQuestion].answers.map((answer, index) =>
+          <ListItem dense button onClick={handleToggle(index)} key={answer.id}>
+            <ListItemIcon>
+              <Checkbox
+                edge="start"
+                checked={answer.checked}
+                tabIndex={-1}
+                disableRipple
+                inputProps={{ 'aria-labelledby': answer.id }}
+              />
+            </ListItemIcon>
+            <ListItemText id={answer.id} primary={answer.content}/>
+          </ListItem>
+        )}
+      </List>
 
-    <Button
-      onClick={handleNext}
-    >
-      Next
-    </Button>
+      <Button
+        onClick={handleNext}
+      >
+        Next
+      </Button>
+    </>}
+    
+    {finished && <>
+      <b>You score is: {points}/{maxPoints}</b>
+    </>}
   </div>
 }
 
--- src/features/quiz/quizSlice.js
+++ src/features/quiz/quizSlice.js
@@ -4,32 +4,33 @@ export const slice = createSlice({
   name: 'quiz',
   initialState: {
     activeQuestion: 0,
+    isFinished: false,
     questions: [
       {
         id: 1,
         content: 'What is React?',
         answers: [
-          { id: 1, content: 'Backend\'s Framework', checked: false },
-          { id: 2, content: 'UI library', checked: false },
-          { id: 3, content: 'Testing library', checked: false },
+          { id: 1, content: 'Backend\'s Framework', checked: false, points: 0 },
+          { id: 2, content: 'UI library', checked: false, points: 1 },
+          { id: 3, content: 'Testing library', checked: false, points: 0 },
         ]
       },
       {
         id: 2,
         content: 'What is Redux?',
         answers: [
-          { id: 1, content: 'Backend\'s Framework', checked: false },
-          { id: 2, content: 'React\'s state management', checked: false },
-          { id: 3, content: 'Testing library', checked: false },
+          { id: 1, content: 'Backend\'s Framework', checked: false, points: 0 },
+          { id: 2, content: 'React\'s state management', checked: false, points: 1 },
+          { id: 3, content: 'Testing library', checked: false, points: 0 },
         ]
       },
       {
         id: 3,
         content: 'What is Vue?',
         answers: [
-          { id: 1, content: 'Backend\'s Framework', checked: false },
-          { id: 2, content: 'UI library', checked: false },
-          { id: 3, content: 'Testing library', checked: false },
+          { id: 1, content: 'Backend\'s Framework', checked: false, points: 0 },
+          { id: 2, content: 'UI library', checked: false, points: 1 },
+          { id: 3, content: 'Testing library', checked: false, points: 0 },
         ]
       }
     ]
@@ -41,6 +42,9 @@ export const slice = createSlice({
     toggle: (state, action) => {
       const question = state.questions[state.activeQuestion];
       question.answers[action.payload].checked = !question.answers[action.payload].checked;
+    },
+    finish: state => {
+      state.isFinished = true
     }
   }
 });
@@ -51,6 +55,28 @@ export const selectActive = state => state.quiz.questions;
 
 export const selectActiveQuestion = state => state.quiz.activeQuestion;
 
-export const { next, toggle } = slice.actions;
+export const selectFinished = state => state.quiz.isFinished;
+
+export const selectMaxPoints = state => {
+  let maxPoints = 0;
+
+  state.quiz.questions.forEach(question => {
+    maxPoints += question.answers.filter(answer => answer.points > 0).length;
+  });
+
+  return maxPoints;
+}
+
+export const selectPoints = state => {
+  let points = 0;
+
+  state.quiz.questions.forEach(question => {
+    points += question.answers.filter(answer => answer.points > 0 && answer.checked).length;
+  });
+
+  return points;
+}
+
+export const { next, toggle, finish } = slice.actions;
 
 export default slice.reducer;
