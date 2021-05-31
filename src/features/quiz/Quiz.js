c879f7448fc7341d20343f6f87c7470d36e6bea1
Mikulew <mikulew@gmail.com>
2021-05-31 22:27:08 +0200

    Add toggle action and answers' list

--- src/features/quiz/Quiz.js
+++ src/features/quiz/Quiz.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { selectActiveQuestion, selectQuestions, next } from './quizSlice';
+import { selectActiveQuestion, selectQuestions, next, toggle } from './quizSlice';
 import { useSelector, useDispatch } from 'react-redux';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
 
 import { Button } from '@material-ui/core'
 
@@ -9,10 +14,27 @@ const Quiz = () => {
   const questions = useSelector(selectQuestions);
   const dispatch = useDispatch();
   const handleNext = () => questions[activeQuestion + 1] ? dispatch(next()) : console.log('finished!');
+  const handleToggle = answerIndex => () => { dispatch(toggle(answerIndex)) };
 
   return <div>
     <p>{activeQuestion + 1} / {questions.length}</p>
     <p>{questions[activeQuestion].content}</p>
+    <List>
+      {questions[activeQuestion].answers.map((answer, index) =>
+        <ListItem dense button onClick={handleToggle(index)} key={answer.id}>
+          <ListItemIcon>
+            <Checkbox
+              edge="start"
+              checked={answer.checked}
+              tabIndex={-1}
+              disableRipple
+              inputProps={{ 'aria-labelledby': answer.id }}
+            />
+          </ListItemIcon>
+          <ListItemText id={answer.id} primary={answer.content}/>
+        </ListItem>
+      )}
+    </List>
 
     <Button
       onClick={handleNext}
--- src/features/quiz/quizSlice.js
+++ src/features/quiz/quizSlice.js
@@ -7,21 +7,40 @@ export const slice = createSlice({
     questions: [
       {
         id: 1,
-        content: 'What is React?'
+        content: 'What is React?',
+        answers: [
+          { id: 1, content: 'Backend\'s Framework', checked: false },
+          { id: 2, content: 'UI library', checked: false },
+          { id: 3, content: 'Testing library', checked: false },
+        ]
       },
       {
         id: 2,
-        content: 'What is Redux?'
+        content: 'What is Redux?',
+        answers: [
+          { id: 1, content: 'Backend\'s Framework', checked: false },
+          { id: 2, content: 'React\'s state management', checked: false },
+          { id: 3, content: 'Testing library', checked: false },
+        ]
       },
       {
         id: 3,
-        content: 'What is Vue?'
+        content: 'What is Vue?',
+        answers: [
+          { id: 1, content: 'Backend\'s Framework', checked: false },
+          { id: 2, content: 'UI library', checked: false },
+          { id: 3, content: 'Testing library', checked: false },
+        ]
       }
     ]
   },
   reducers: {
     next: state => {
       state.activeQuestion += 1
+    },
+    toggle: (state, action) => {
+      const question = state.questions[state.activeQuestion];
+      question.answers[action.payload].checked = !question.answers[action.payload].checked;
     }
   }
 });
@@ -32,6 +51,6 @@ export const selectActive = state => state.quiz.questions;
 
 export const selectActiveQuestion = state => state.quiz.activeQuestion;
 
-export const { next } = slice.actions;
+export const { next, toggle } = slice.actions;
 
 export default slice.reducer;
